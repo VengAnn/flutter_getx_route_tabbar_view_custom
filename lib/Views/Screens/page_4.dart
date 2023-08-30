9c755794b5c91a22995623e9fa7c4c8d6a5e1f8e
VengAnn <venganncoco@gmail.com>
2023-08-30 16:36:52 +0700

    update

--- lib/Views/Screens/page_4.dart
+++ lib/Views/Screens/page_4.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:get/get.dart';
 
 class Page4 extends StatelessWidget {
@@ -21,6 +22,20 @@ class Page4 extends StatelessWidget {
                 Get.back(); //go back like Navigator.pop(context);
               },
               child: const Text("Back Home"),
+            ),
+            //
+            RatingBar.builder(
+              initialRating: 3.5,
+              minRating: 0.5,
+              direction: Axis.horizontal,
+              allowHalfRating: true,
+              itemCount: 5,
+              itemSize: 30,
+              itemBuilder: (context, index) => const Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {},
             ),
           ],
         ),
